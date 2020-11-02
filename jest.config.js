e57eaad68942321f3a3e66465a54e4e71c15ba65
Dan Hunt <danhunt@rockcentraldetroit.com>
2020-11-02 13:56:56 -0500

    fix: coverage ignore paths

--- jest.config.js
+++ jest.config.js
@@ -15,14 +15,17 @@ module.exports = {
     '!src/**/*.d.ts',
   ],
   coveragePathIgnorePatterns: [
-    "node_modules"
+    'node_modules',
+    '<rootDir>/src/interfaces/index.ts',
+    '<rootDir>/src/services/index.ts',
+    '<rootDir>/src/index.ts'
   ],
   coverageThreshold: {
-    "global": {
-        "branches": 85,
-        "functions": 85,
-        "lines": 85,
-        "statements": 85
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85
     }
   }
 };
