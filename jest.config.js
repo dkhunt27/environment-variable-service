4fe404900e5af90a8c4dbaddd277024aabc84c29
Dan Hunt <danhunt@rockcentraldetroit.com>
2020-11-02 13:51:12 -0500

    fix: coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,15 @@ module.exports = {
     'src/**/*.{ts,tsx,js,jsx}',
     '!src/**/*.d.ts',
   ],
+  coveragePathIgnorePatterns: [
+    "node_modules"
+  ],
+  coverageThreshold: {
+    "global": {
+        "branches": 85,
+        "functions": 85,
+        "lines": 85,
+        "statements": 85
+    }
+  }
 };
